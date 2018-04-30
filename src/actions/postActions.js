a8c4b37f65e3b64b1a99f21f63a325151781beff
Sergey Mazur <mazzurs@ya.ru>
2018-04-30 19:20:55 +0300

    Added some things

---
+++ src/actions/postActions.js
@@ -0,0 +1,10 @@
+import { FETCH_POSTS, NEW_POST } from '../actions/types'
+
+export const fetchPosts = () => dispatch => {
+    fetch('http://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(posts => dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      }))
+}
